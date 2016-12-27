3a43b72428869596d73e342beed1da5db3951b6a
Nicholas Ketchum <nketchum@gmail.com>
2016-12-27 14:40:29 -0800

    boil it all down to colorize.sh

--- colorize.sh
+++ colorize.sh
@@ -12,7 +12,7 @@ frames_mono="$frames_mono_dir/*.png";
 frames_color="$frames_color_dir/*.png";
 
 # Input-output
-input="$rootdir/input/76409047.mp4";
+input="$rootdir/input/n5UnEB23YCI.mp4";
 output_dir="$rootdir/output";
 output="$output_dir/out.mp4";
 
@@ -22,7 +22,10 @@ fps=24;
 # Create proc directories.
 rm -rf "$frames_mono_dir" "$frames_color_dir" "$output_dir" || true;
 mkdir -p "$frames_mono_dir" "$frames_color_dir" "$output_dir";
-chmod -R 777 "$frames_mono_dir" "$frames_color_dir" "$output_dir";
+# chmod -R 777 "$frames_mono_dir" "$frames_color_dir" "$output_dir";
+
+# Grab remote video
+#youtube-dl https://www.youtube.com/watch?v=n5UnEB23YCI -f 'bestvideo[height<=480]+bestaudio/best[height<=480]'
 
 # Make monochrome frames from source vid.
 /usr/local/bin/ffmpeg -i "$input" -r $fps "$frames_mono_dir/%04d.png";
